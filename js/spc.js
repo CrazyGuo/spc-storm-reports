b7c2f795d0758ed0b52c25f0069b6c3c1759f5d8
brendan <brendan.heberton@gmail.com>
2013-05-27 12:21:29 -0600

    0527

--- js/spc.js
+++ js/spc.js
@@ -88,7 +88,7 @@ stormReports.prototype.loadReports = function() {
   var self = this;
   
   //d3.csv("http://www.spc.noaa.gov/climo/reports/today_torn.csv")
-   d3.csv('data/0524_wind.csv')
+   d3.csv('data/yesterday_wind_1.csv')
     .row(function(d) { return { time: d.Time, scale: d.F_Scale, location: d.Location, county: d.County,
       state: d.State, latitude: d.Lat, longitude: d.Lon, comments: d.Comments, type: "Reported Wind"}; })
     .get(function(error, rows) {
@@ -116,7 +116,7 @@ stormReports.prototype.loadReports = function() {
         });
      });
      
-    d3.csv('data/0524_hail.csv')
+    d3.csv('data/yesterday_hail_1.csv')
     .row(function(d) { return { time: d.Time, scale: d.F_Scale, location: d.Location, county: d.County,
       state: d.State, latitude: d.Lat, longitude: d.Lon, comments: d.Comments, type: 'Reported Hail'}; })
     .get(function(error, rows) {
@@ -144,8 +144,7 @@ stormReports.prototype.loadReports = function() {
         });
      });
      
-   /*
-   d3.csv('data/20_torn.csv')
+   d3.csv('data/yesterday_torn_1.csv')
     .row(function(d) { return { time: d.Time, scale: d.F_Scale, location: d.Location, county: d.County,
       state: d.State, latitude: d.Lat, longitude: d.Lon, comments: d.Comments, type: 'Reported Tornado'}; })
     .get(function(error, rows) {
@@ -173,7 +172,6 @@ stormReports.prototype.loadReports = function() {
         });
         
      });
-   */
 }
 
 stormReports.prototype.hover = function(d) { 
